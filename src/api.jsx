880e924b5a67ec4038f8287c425d533c5d945683
Mendozaa17 <humbertomendoza2017@gmail.com>
2023-01-10 20:49:44 -0600

    Weather project working with OpenWeatherMaps and GeoDB

---
+++ src/api.jsx
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c125450bbfmsh59f14be020aa6bap107874jsn82cfd2fe3c24',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "ed5fd85f7b4a50fdce3672b41c7a8ec2"
